42c2c616ddd644a34a6cf1c93f98c6d47c9d396f
Artur Gaster <gasterartur@googlemail.com>
2024-11-25 11:36:06 +0100

    Add Contacts

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styles from "./Cart.module.css";
+import { useCartContext } from "../../context/CartContextProvider";
+import CartCard from "../CartCard/CartCard";
+
+export default function Cart() {
+  const context = useCartContext();
+
+  return (
+    <div className={styles.Wrapper}>
+      <div className={styles.CartList}>
+        {context.cartItems.map((item) => (
+          <CartCard key={item.id} product={item} />
+        ))}
+      </div>
+      <CartTotals cartItems={context.cartItems} />
+    </div>
+  );
+}
